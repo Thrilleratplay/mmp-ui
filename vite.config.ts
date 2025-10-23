bcbc4e9a77db744cd7c103792682cef1a04db228
Tom Hiller <thrilleratplay@gmail.com>
2025-10-23 13:08:51 -0400

    chore(vite.config): replace postcss.config.cjs with vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import checker from 'vite-plugin-checker';
+import postCssNested from 'postcss-nested';
+import postCssAutoPrefixer from 'autoprefixer';
+import postCssSimpleVars from 'postcss-simple-vars';
+import postCssMantinePreset from 'postcss-preset-mantine';
+import postCssPxToRem from 'postcss-pxtorem';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-        checker({
+    checker({
       typescript: true,
       stylelint: {
         lintCommand: 'stylelint src/**/*.css',
@@ -18,8 +23,28 @@ export default defineConfig({
         initialIsOpen: false,
       },
     }),
-
   ],
+  css: {
+    postcss: {
+      plugins: [
+        postCssNested(),
+        postCssAutoPrefixer(),
+        postCssMantinePreset(),
+        postCssPxToRem({
+          replace: false
+        }),
+        postCssSimpleVars({
+          variables: {
+            'mantine-breakpoint-xs': '36em',
+            'mantine-breakpoint-sm': '48em',
+            'mantine-breakpoint-md': '62em',
+            'mantine-breakpoint-lg': '75em',
+            'mantine-breakpoint-xl': '88em',
+          }
+        }),
+      ],
+    }
+  },
   resolve: {
     alias: [
       { find: '@', replacement: '/src' },
